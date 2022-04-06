d13ca0e31ec78972a902c1d59ee3d01218fbd3a3
robin.dandrea <robin.dandrea@gmail.com>
2022-04-06 12:51:04 +0200

    removed obsolete trades method

--- LemonMarkets.Sdk/Interfaces/ITradesRepo.cs
+++ LemonMarkets.Sdk/Interfaces/ITradesRepo.cs
@@ -9,9 +9,6 @@ namespace LemonMarkets.Interfaces
     public interface ITradesRepo
     {
 
-        [Obsolete("Please use GetLatestAsync, diese Methode wird noch bis zum 2022-04-01 funktionieren")]
-        Task<LemonResults<Trade>> GetAsync ( TradesSearchFilter request );
-
         Task<LemonResults<Trade>> GetLatestAsync ( TradesLatestSearchFilter request );
 
     }
